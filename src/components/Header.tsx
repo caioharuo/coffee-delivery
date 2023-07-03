5765b46e23cd3f2527db37718e8ad4b032317d47
Caio Haruo <caio.haruo@hotmail.com>
2023-07-03 12:40:03 -0300

    chore: Replace "a" tag to "Link" from react-router-dom

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
-import logoImg from '../assets/logo.svg';
 import { MapPin, ShoppingCart } from 'phosphor-react';
+import { Link } from 'react-router-dom';
+import logoImg from '../assets/logo.svg';
 
 export function Header() {
   return (
     <header className="flex items-center justify-between max-w-[1120px] mx-auto py-8">
-      <a href="/">
+      <Link to="/">
         <img src={logoImg} alt="" />
-      </a>
+      </Link>
 
       <div className="flex gap-3">
         <div className="flex bg-purple-100 p-2 gap-1 rounded-lg">
@@ -14,9 +15,9 @@ export function Header() {
           <span className="text-purple-800 text-sm">Porto Alegre, RS</span>
         </div>
 
-        <a href="/checkout" className="bg-yellow-100 p-2 gap-1 rounded-lg">
+        <Link to="/checkout" className="bg-yellow-100 p-2 gap-1 rounded-lg">
           <ShoppingCart className="text-yellow-700" size={22} weight="fill" />
-        </a>
+        </Link>
       </div>
     </header>
   );
